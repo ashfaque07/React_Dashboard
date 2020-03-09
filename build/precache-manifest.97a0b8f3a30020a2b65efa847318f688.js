932d6e64b83f493dcb52b4c28e4d1b2044748f61
Ashfaque <ashfaque.b.shaikh@accenture.com>
2020-03-09 16:30:49 +0530

    initial commit

---
+++ build/precache-manifest.97a0b8f3a30020a2b65efa847318f688.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d5cba525b672a8738c1a17981621f00",
+    "url": "/React_Dashboard_2/index.html"
+  },
+  {
+    "revision": "2b28f4780e7dcb6119d5",
+    "url": "/React_Dashboard_2/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "90bc0b90943b0e055119",
+    "url": "/React_Dashboard_2/static/css/main.cce1381e.chunk.css"
+  },
+  {
+    "revision": "2b28f4780e7dcb6119d5",
+    "url": "/React_Dashboard_2/static/js/2.e038513f.chunk.js"
+  },
+  {
+    "revision": "36edae049372e7c14c35ea2fa992212f",
+    "url": "/React_Dashboard_2/static/js/2.e038513f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90bc0b90943b0e055119",
+    "url": "/React_Dashboard_2/static/js/main.d922f6e5.chunk.js"
+  },
+  {
+    "revision": "772efe5590ad79986fbf3a1db778be8b",
+    "url": "/React_Dashboard_2/static/js/main.d922f6e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026bc73acea9933e961d",
+    "url": "/React_Dashboard_2/static/js/runtime-main.1b42145b.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/React_Dashboard_2/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/React_Dashboard_2/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/React_Dashboard_2/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/React_Dashboard_2/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/React_Dashboard_2/static/media/face-3.ce7a6b79.jpg"
+  },
+  {
+    "revision": "9b864b3680f372ff9ae02649fbc44838",
+    "url": "/React_Dashboard_2/static/media/reactlogo.9b864b36.png"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/React_Dashboard_2/static/media/sidebar-1.6be21e8a.jpg"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/React_Dashboard_2/static/media/sidebar-2.7a4ce7cc.jpg"
+  },
+  {
+    "revision": "34535b958ad0f0183770a8731b31b4ca",
+    "url": "/React_Dashboard_2/static/media/sidebar-3.34535b95.jpg"
+  },
+  {
+    "revision": "7bc259fd5cef15e95376c822c7e23081",
+    "url": "/React_Dashboard_2/static/media/sidebar-4.7bc259fd.jpg"
+  }
+]);
